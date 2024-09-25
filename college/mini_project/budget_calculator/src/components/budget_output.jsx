2fbee3f19633ddb5c4eb5cc91f155da6bf3ed39e
Roshan-Devassy <roshandevassy101112@gmail.com>
2024-09-25 13:48:16 +0530

    updated budget output  responsive

--- college/mini_project/budget_calculator/src/components/budget_output.jsx
+++ college/mini_project/budget_calculator/src/components/budget_output.jsx
@@ -8,46 +8,46 @@ const Budget_output = (props) => {
     <>
       <section className="font-serif text-lg 2xl:text-xl 3xl:text-2xl">
         {/* Budget Output Header */}
-        <section className=" p-1 xsm:p-5 flex gap-5 justify-center md:justify-start items-center border-black border-b flex-wrap sm:flex-nowrap flex-grow">
+        <section className="p-1 xsm:p-5 flex flex-col sm:flex-row gap-1 xsm:gap-2 sm:gap-5 justify-center items-center border-black border-b flex-wrap sm:flex-nowrap flex-grow"/* "p-1 xsm:p-5 flex gap-1 xsm:gap-2 justify-center md:justify-start items-center border-black border-b flex-wrap sm:flex-nowrap flex-grow" */>
           <div className="">
-            <img src={piggy} className="h-8 w-8 sm:h-12 sm:w-12" />
+            <img src={piggy} className="h-6 w-6 xsm:h-8 xsm:w-8 sm:h-12 sm:w-12 3xl:h-14 3xl:w-14"/* "h-8 w-8 sm:h-12 sm:w-12" */ />
           </div>
           <div className="">
-            <h2 className="text-orange-600 font-semibold text-center md:text-left lg:text-lg 2xl:text-xl 3xl:text-2xl">
-              My <span className="font-sans">50/30/20</span> Monthly Budget
+            <h2 className="text-orange-600 font-semibold text-center md:text-left text-xs xsm:text-sm sm:text-lg md:text-xl lg:text-2xl 2xl:text-2xl 3xl:text-3xl">
+              My <span className="font-sans font-semibold">50/30/20</span> Monthly Budget
             </h2>
           </div>
         </section>
         {/* Budget Output Images */}
         <section className="">
-          <div className="pt-3 font-sans text-lg mb-5">
-            <p className=" text-center 2xl:text-xl 3xl:text-2xl">
+          <div className="pt-3 font-sans text-lg mb-1 xsm:mb-3 sm:mb-4 xmd:mb-5">
+            <p className="text-xs xsm:text-sm sm:text-base xl:text-lg text-center"/* " text-center 2xl:text-xl 3xl:text-2xl" */>
               Your Monthly Income {Math.ceil(props.monthly_income).toFixed(2)}
               &#8377;
             </p>
           </div>
-          <div className="flex flex-col md:flex-row items-center md:justify-around p-2 flex-wrap">
-            <div className="flex flex-col justify-center gap-3">
-              <img src={house} className="h-16 w-16 self-center" />
-              <p className=" text-center">
+          <div className="flex flex-col xmd:flex-row items-center xmd:justify-around p-1 gap-2 xsm:gap-3 sm:gap-4 xmd:gap-5 flex-wrap">
+            <div className="flex flex-col justify-center gap-1 xsm:gap-2">
+              <img src={house} className="h-6 w-6 xsm:h-8 xsm:w-8 sm:h-12 sm:w-12 3xl:h-14 3xl:w-14 self-center"/* "h-16 w-16 self-center" */ />
+              <p className="text-xs xsm:text-sm sm:text-base xl:text-lg text-center">
                 Needs{" "}
                 <span className="font-sans">
                   50% &#8377;{Math.ceil(props.fifty).toFixed(2)}
                 </span>
               </p>
             </div>
-            <div className="flex flex-col justify-center gap-3 py-10 md:py-0">
-              <img src={giftbox} className="h-16 w-16 self-center" />
-              <p className=" text-center">
+            <div className="flex flex-col justify-center gap-1 xsm:gap-2">
+              <img src={giftbox} className="h-6 w-6 xsm:h-8 xsm:w-8 sm:h-12 sm:w-12 3xl:h-14 3xl:w-14 self-center" />
+              <p className="text-xs xsm:text-sm sm:text-base xl:text-lg text-center">
                 Wants{" "}
                 <span className="font-sans">
                   30% &#8377;{Math.ceil(props.thirty).toFixed(2)}
                 </span>
               </p>
             </div>
-            <div className="flex flex-col justify-center gap-3">
-              <img src={savings} className="h-16 w-16 self-center" />
-              <p className=" text-center">
+            <div className="flex flex-col justify-center gap-1 xsm:gap-2">
+              <img src={savings} className="h-6 w-6 xsm:h-8 xsm:w-8 sm:h-12 sm:w-12 3xl:h-14 3xl:w-14 self-center" />
+              <p className="text-xs xsm:text-sm sm:text-base xl:text-lg text-center">
                 Savings{" "}
                 <span className="font-sans">
                   20% &#8377;{Math.ceil(props.twenty).toFixed(2)}
@@ -58,12 +58,12 @@ const Budget_output = (props) => {
         </section>
         {/* Budget Output Content */}
         <section>
-          <div className="flex justify-center pt-5 pb-2">
-            <p className="text-slate-500 text-center">
+          <div className="flex justify-center mt-3 xsm:mt-4  sm:mt-5 sm:mb-2">
+            <p className="text-slate-500 text-xs xsm:text-sm sm:text-base xl:text-lg text-center">
               Values are based on a monthly budget
             </p>
           </div>
-          <div className="flex flex-col gap-3 flex-wrap text-justify p-2">
+          <div className="flex flex-col gap-2  flex-wrap text-justify p-2 text-xs xsm:text-sm sm:text-base xl:text-lg">
             <p>Congratulations, you're off to a great start!</p>
             <p>
               Your <span className="font-sans">50/30/20</span> budget is a
