0a82a6a5da619669cb8c64bd3f19408c6f6facc5
Roshan-Devassy <roshandevassy101112@gmail.com>
2024-09-26 21:25:18 +0530

    updated budget calculation  responsive

--- college/mini_project/budget_calculator/src/components/budget_calculation.jsx
+++ college/mini_project/budget_calculator/src/components/budget_calculation.jsx
@@ -119,24 +119,24 @@ const Budget_calculation = (props) => {
 
           {/*Needs, Wants & Savings wrapper */}
           <div
-            className=" flex flex-col self-center flex-wrap  md:flex-row *:flex-grow gap-2 w-full sm:gap-5 lg:gap-7 mt-4 xl:mt-6 2xl:mt-8 3xl:mt-10" /* "md:flex md:justify-around md:items-center" */
+            className="flex flex-col self-center flex-wrap  md:flex-row *:flex-grow gap-2 w-full sm:gap-5 lg:gap-7 mt-3 xsm:mt-4 sm:mt-5 xl:mt-6 2xl:mt-8 3xl:mt-10"/* " flex flex-col self-center flex-wrap  md:flex-row *:flex-grow gap-2 w-full sm:gap-5 lg:gap-7 mt-3 xsm:mt-4 sm:mt-5 xl:mt-6 2xl:mt-8 3xl:mt-10" */ /* "md:flex md:justify-around md:items-center" */
           >
             {/* Needs wrapper*/}
             <div className="flex flex-col border border-black rounded-md sm:self-center">
               <section
-                className="flex mb-2 gap-3 items-center justify-center rounded-md border-black border-x-8 border-b" /* "grid grid-cols-2 row-span-2 justify-self-center items-center gap-1 mb-1" */ /* "flex gap-3 items-center pb-2" */
+                className="flex mb-2 gap-3 p-1 items-center justify-center rounded-md border-black border-x-8 border-b text-xs xsm:text-sm sm:text-base md:text-lg lg:text-xl 3xl:text-2xl" /* "grid grid-cols-2 row-span-2 justify-self-center items-center gap-1 mb-1" */ /* "flex gap-3 items-center pb-2" */
               >
                 <img
                   src={house}
                   alt="house"
-                  className="h-5 w-5 xsm:h-6 xsm:w-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8"
+                  className="h-4 w-4 xsm:h-5 xsm:w-5 sm:h-7 sm:w-7"/* "h-5 w-5 xsm:h-6 xsm:w-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8" */
                 />
-                <p className="font-semibold text-sm xsm:text-base sm:text-lg lg:text-xl font-serif">
+                <p className="font-semibold font-serif">
                   Needs
                 </p>
               </section>
               <section
-                className="flex flex-col sm:self-center gap-1 text-sm xsm:text-base lg:text-lg font-mono font-semibold md:w-full p-1 *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center" /* "flex flex-col gap-1" */ /* "grid grid-rows-3 pb-5 gap-1" */ /* "grid grid-rows-3 pb-5 gap-1 md:flex flex-col" */
+                className="flex flex-col gap-3 p-1 sm:self-center text-xs xsm:text-sm sm:text-base lg:text-lg 3xl:text-xl font-mono font-semibold md:w-full  *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center" /* "flex flex-col gap-1" */ /* "grid grid-rows-3 pb-5 gap-1" */ /* "grid grid-rows-3 pb-5 gap-1 md:flex flex-col" */
               >
                 <div
                   className="" /* "grid grid-cols-2 gap-2 bg-slate-100" */ /*  "grid grid-cols-2 place-self-center gap-x-20" */
@@ -145,7 +145,7 @@ const Budget_calculation = (props) => {
                   <p>&#8377;{Math.ceil(props.fifty).toFixed(2)} </p>
                 </div>
                 <div
-                  className="py-1" /* "grid grid-cols-2 bg-slate-100" */ /* "flex justify-around items-center flex-wrap" */ /* "grid grid-cols-2 place-self-center " */ /* "flex justify-between items-center flex-wrap" */
+                  className="" /* "grid grid-cols-2 bg-slate-100" */ /* "flex justify-around items-center flex-wrap" */ /* "grid grid-cols-2 place-self-center " */ /* "flex justify-between items-center flex-wrap" */
                 >
                   <p>My Budget</p>
                   <p>&#8377;{Math.ceil(props.needs_total).toFixed(2)}</p>
@@ -160,24 +160,24 @@ const Budget_calculation = (props) => {
             </div>
             {/* Wants Wrapper*/}
             <div className="flex flex-col border border-black rounded-md sm:self-center">
-              <section className="flex gap-3 items-center justify-center mb-2 border-b border-black border-x-8 rounded-md">
+              <section className="flex gap-3 p-1 items-center justify-center mb-2 border-b border-black border-x-8 rounded-md">
                 <img
                   src={giftbox}
                   alt="house"
-                  className="h-5 w-5 xsm:h-6 xsm:w-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8"
+                  className="h-4 w-4 xsm:h-5 xsm:w-5 sm:h-7 sm:w-7"
                 />
                 <p className="font-serif font-semibold text-sm xsm:text-base sm:text-lg lg:text-xl">
                   Wants
                 </p>
               </section>
               <section
-                className="flex flex-col gap-1 text-sm xsm:text-base lg:text-lg basis-16 flex-shrink p-1 *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center px-[2px] font-mono font-semibold" /* "flex flex-col gap-1" */ /* "grid grid-rows-3 pb-5 gap-1 " */
+                className="flex flex-col gap-3 text-sm xsm:text-base lg:text-lg basis-16 flex-shrink p-1 *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center px-[2px] font-mono font-semibold" /* "flex flex-col gap-1" */ /* "grid grid-rows-3 pb-5 gap-1 " */
               >
                 <div className="">
                   <p>50/30/20</p>
                   <p>&#8377;{Math.ceil(props.thirty).toFixed(2)}</p>
                 </div>
-                <div className="py-1">
+                <div className="">
                   <p>My Budget</p>
                   <p>&#8377;{Math.ceil(props.wants_total).toFixed(2)}</p>
                 </div>
@@ -189,24 +189,24 @@ const Budget_calculation = (props) => {
             </div>
             {/* Savings Wrapper*/}
             <div className="flex flex-col border border-black rounded-md sm:self-center">
-              <section className="flex gap-3 items-center justify-center mb-2 border-black border-b border-x-8 rounded-md">
+              <section className="flex gap-3 p-1 items-center justify-center mb-2 border-black border-b border-x-8 rounded-md">
                 <img
                   src={savings}
                   alt="savings"
-                  className="h-5 w-5 xsm:h-6 xsm:w-6 sm:w-7 sm:h-7 lg:h-8 lg:w-8"
+                  className="h-4 w-4 xsm:h-5 xsm:w-5 sm:h-7 sm:w-7"
                 />
                 <p className="font-serif font-semibold text-sm xsm:text-base sm:text-lg lg:text-xl">
                   Savings
                 </p>
               </section>
               <section
-                className="flex flex-col gap-1 text-sm xsm:text-base lg:text-lg basis-16 flex-shrink p-1 *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center px-[2px] font-mono font-semibold" /* "grid grid-rows-3 gap-2" */
+                className="flex flex-col gap-3 text-sm xsm:text-base lg:text-lg basis-16 flex-shrink p-1 *:flex *:flex-col *:items-center *:sm:flex-row *:sm:justify-between *:sm:items-center px-[2px] font-mono font-semibold" /* "grid grid-rows-3 gap-2" */
               >
                 <div className="">
                   <p>50/30/20</p>
                   <p>&#8377;{Math.ceil(props.twenty).toFixed(2)}</p>
                 </div>
-                <div className="py-1">
+                <div className="">
                   <p>My Budget</p>
                   <p>&#8377;{Math.ceil(props.savings_total).toFixed(2)} </p>
                 </div>
